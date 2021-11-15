3adafae05ce2092f716445ab483eb8115efd0187
nathanhepke <n.hepke@newcastle.ac.uk>
2021-11-15 15:03:51 +0000

    Merged all enrolment files together with columns I need

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,56 @@
 # Example preprocessing script.
+View(cyber_security_1_enrolments)
+View(cyber_security_6_enrolments)
+View(cyber_security_7_enrolments)
+
+fully_participated_run1 = filter(cyber_security_1_enrolments, fully_participated_at != "NA",
+                                 gender != "Unknown", country != "Unknown", age_range != "Unknown",
+                                 highest_education_level != "Unknown", employment_status != "Unknown",
+                                 employment_area != "Unknown")
+
+fully_participated_run2 = filter(cyber_security_2_enrolments, fully_participated_at != "NA",
+                                 gender != "Unknown", country != "Unknown", age_range != "Unknown",
+                                 highest_education_level != "Unknown", employment_status != "Unknown",
+                                 employment_area != "Unknown")
+
+fully_participated_run3 = filter(cyber_security_3_enrolments, fully_participated_at != "NA",
+                                 gender != "Unknown", country != "Unknown", age_range != "Unknown",
+                                 highest_education_level != "Unknown", employment_status != "Unknown",
+                                 employment_area != "Unknown")
+
+fully_participated_run4 = filter(cyber_security_4_enrolments, fully_participated_at != "NA",
+                                 gender != "Unknown", country != "Unknown", age_range != "Unknown",
+                                 highest_education_level != "Unknown", employment_status != "Unknown",
+                                 employment_area != "Unknown")
+
+fully_participated_run5 = filter(cyber_security_5_enrolments, fully_participated_at != "NA",
+                                 gender != "Unknown", country != "Unknown", age_range != "Unknown",
+                                 highest_education_level != "Unknown", employment_status != "Unknown",
+                                 employment_area != "Unknown")
+
+fully_participated_run6 = filter(cyber_security_6_enrolments, fully_participated_at != "NA",
+                                 gender != "Unknown", country != "Unknown", age_range != "Unknown",
+                                 highest_education_level != "Unknown", employment_status != "Unknown",
+                                 employment_area != "Unknown")
+
+fully_participated_run7 = filter(cyber_security_7_enrolments, fully_participated_at != "NA",
+                                 gender != "Unknown", country != "Unknown", age_range != "Unknown",
+                                 highest_education_level != "Unknown", employment_status != "Unknown",
+                                 employment_area != "Unknown")
+
+fully_participated_run1
+fully_participated_run2
+fully_participated_run3
+fully_participated_run4
+fully_participated_run5
+fully_participated_run6
+fully_participated_run7
+
+fully_participated_runs = merge(fully_participated_run1,fully_participated_run2, all = TRUE)
+fully_participated_runs = merge(fully_participated_runs, fully_participated_run3, all = TRUE)
+fully_participated_runs = merge(fully_participated_runs, fully_participated_run4, all = TRUE)
+fully_participated_runs = merge(fully_participated_runs, fully_participated_run5, all = TRUE)
+fully_participated_runs = merge(fully_participated_runs, fully_participated_run6, all = TRUE)
+fully_participated_runs = merge(fully_participated_runs, fully_participated_run7, all = TRUE)
+
+fully_participated_runs
